209ab5bfcb96762e4ad9cd4537b78e97d264f23d
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-17 17:20:42 -0400

    updating the Login, forget password, signup page

--- src/components/Landingpage/CreateAccount/Accounts.jsx
+++ src/components/Landingpage/CreateAccount/Accounts.jsx
@@ -4,8 +4,8 @@ import { FiX } from 'react-icons'
 export const Signin = () => {
     return (
         <div>
-            <div className='bg-white rounded-2xl p-3'>
-                <form action="">
+            <div className='bg-white rounded-2xl p-3 w-[30%]'>
+                <form className='flex flex-col gap-3' action="">
                     <div className='flex w-full justify-between'>
                         <h1>Create Account</h1>
                         <FiX />
@@ -29,12 +29,50 @@ export const Signin = () => {
                     <div>
                         <button className='p-2 text-white'>Sign Up</button>
                         <p>or</p>
-                        <div>
-                            
-                        </div>
+                        <button>
+                            <p>G</p>
+                            <p>Sign Up with Google</p>
+                        </button>
                     </div>
+                    <p>Already have an Account? <Link className="text-[orange]" href="">Login</Link></p>
                 </form>
             </div>
         </div>
     )
 }
+
+import React from 'react'
+
+export const Login = () => {
+    return (
+        <div>
+            <div className='bg-white rounded-2xl p-3 w-[30%]'>
+                <form className='flex flex-col gap-3' action="">
+                    <div className='flex w-full justify-between'>
+                        <h1>Login</h1>
+                        <FiX />
+                    </div>
+                    <span className='flex flex-col gap-2'>
+                        <label htmlFor="">Email Address</label>
+                        <input className='w-full p-1' type="email" />
+                    </span>
+                    <span className='flex flex-col gap-2'>
+                        <label htmlFor="">Password</label>
+                        <input className='w-full p-1' type="password" />
+                    </span>
+                    <p>Forget your password?</p>
+                    <div>
+                        <button className='p-2 text-white'>Sign In</button>
+                        <p>or</p>
+                        <button>
+                            <p>G</p>
+                            <p>Sign Up with Google</p>
+                        </button>
+                    </div>
+                    <p>Don't have an Account? <Link className="text-[orange]" href="">Sign Up</Link></p>
+                </form>
+            </div>
+        </div>
+    )
+}
+
