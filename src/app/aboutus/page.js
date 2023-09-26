d267db9e7d9706582e8d01be63fbc4515ea7834f
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-26 17:51:41 -0400

    updated about us

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,4 +1,7 @@
+import Footer from '@/components/Landingpage/Footer'
 import Header from '@/components/Landingpage/Header'
+import Testimony from '@/components/Landingpage/Testimony/Testimony'
+import AboutCard from '@/components/aboutus/AboutCard'
 import AboutLuca from '@/components/aboutus/AboutLuca'
 import Hero from '@/components/aboutus/Hero'
 
@@ -10,6 +13,9 @@ const page = () => {
       {/* <Header/> */}
       <Hero/>
       <AboutLuca/>
+      <AboutCard/>
+      <Testimony/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/aboutus/AboutCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { GrMapLocation } from 'react-icons/gr'
+import { FaAward } from 'react-icons/fa'
+import { GiPoliceOfficerHead } from 'react-icons/gi'
+import { MdPriceCheck } from 'react-icons/md'
+
+
+const AboutCard = () => {
+  return (
+    <div className='bg-[url(/Images/orange-bg.svg)] bg-no-repeat gap-8 bg-center grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 px-[10%]'>
+        <div className='bg-[#FED] p-3 rounded-lg flex flex-col items-center'>
+            <GrMapLocation className='text-3xl text-[orange]'/>
+            <b className='capitalize w-[60%] text-center leading-loose'>Complete Packages for all your Wishes</b>
+        </div>
+        <div className='bg-[#FED] p-3 rounded-lg flex flex-col items-center'>
+            <FaAward className='text-3xl text-[orange]'/>
+            <b className='capitalize w-[60%] text-center leading-loose'>Over 30 Years Of Experience</b>
+        </div>
+        <div className='bg-[#FED] p-3 rounded-lg flex flex-col items-center'>
+            <GiPoliceOfficerHead className='text-3xl text-[orange]'/>
+            <b className='capitalize w-[60%] text-center leading-loose'>Expert Guides For You</b>
+        </div>
+        <div className='bg-[#FED] p-3 rounded-lg flex flex-col items-center'>
+            <MdPriceCheck className='text-3xl text-[orange]'/>
+            <b className='capitalize w-[60%] text-center leading-loose'>Guaranteed fun at the best price!</b>
+        </div>
+        
+    </div>
+  )
+}
+
+export default AboutCard
